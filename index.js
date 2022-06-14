1e4c0db507c4e9fdca9b3941f4473a2a036ed69d
Alessandro Souza <alessandrosouzadasilva506@gmail.com>
2022-06-14 19:43:35 -0300

    Atividade S3-16

---
+++ index.js
@@ -0,0 +1,41 @@
+{
+    console.log("Exercício 1")
+
+    let idade = 28
+    let avaliarIdade = ""
+
+    if(idade > 18 && idade < 25){
+        console.log("A idade está entre 18 e 25")
+    } else {
+        console.log("A idade não está entre 18 e 25")
+    }
+}
+
+{
+
+    console.log("Exercício 2")
+
+    let carroNome = "Chevrolet"
+    let carroAno = 2019
+    let filtrarCarro = ""
+
+    if(carroNome == "Chevrolet" && carroAno == 2019){
+        console.log("Carro encontrado")
+    }else{
+        console.log("Carro não encontrado")
+    }
+}
+
+{
+
+    console.log("Exercício 3")
+
+    let botaoMenu = true
+    let avaliarClick = ""
+    if(botaoMenu == false){
+        console.log("Abrir menu")
+    }else{
+        console.log("Fechar menu")
+    }
+
+}
